044a483f9b200e05eb742b7871bf5a62dbb48d1b
strongdevlancer <jasperkoster687@gmail.com>
2023-09-11 20:05:11 -0500

    added dependencies

--- App.tsx
+++ App.tsx
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { PaperProvider } from "react-native-paper";
+import StartPage from "./src/views/StartPage";
 
+const Stack = createNativeStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <PaperProvider>
+        <Stack.Navigator initialRouteName="StartPage">
+          <Stack.Screen name="StartPage" component={StartPage} />
+        </Stack.Navigator>
+        <StatusBar style="auto" />
+      </PaperProvider>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/types/token.ts
@@ -0,0 +1,15 @@
+type token = {
+    "coins_count":number,
+    "active_markets":number,
+    "total_mcap":number,
+    "total_volume":number,
+    "btc_d":string,
+    "eth_d":string,
+    "mcap_change":string,
+    "volume_change":string,
+    "avg_change_percent":number,
+    "volume_ath":number,
+    "mcap_ath":number
+}
+
+export default token;
